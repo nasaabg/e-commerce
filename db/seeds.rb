3d76fc3f376fd1a7347eeca5d47751838cbb4ce8
Janek Kurzydlo <janek.kurzydlo@gmail.com>
2017-01-24 21:24:24 +0100

    Update Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 Product.delete_all
 Category.delete_all
+OrderStatus.delete_all
 
 book = Category.create(name: "Books")
 car = Category.create(name: "Cars")
@@ -12,16 +13,22 @@ ent = Category.create(name: "Entertainment")
 home = Category.create(name: "Home & Garden")
 puts "CATEGORIES CREATED"
 
-Product.create(category: ele, name: "Apple Iphone", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
-Product.create(category: ele, name: "Apple Macbook", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
-Product.create(category: book, name: "Harry Potter", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
-Product.create(category: fasion, name: "T-Shirt", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
-Product.create(category: toys, name: "Lego Army", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
-Product.create(category: sport, name: "Nike Vapor", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
-Product.create(category: art, name: "Kossak", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
-Product.create(category: ent, name: "Justin Bieber", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
-Product.create(category: home, name: "Modern Lamp", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
-Product.create(category: car, name: "Porshe", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
-Product.create(category: car, name: "Volvo", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
-Product.create(category: car, name: "Skoda", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10))
+Product.create(category: ele, name: "Apple Iphone", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10), active: true)
+Product.create(category: ele, name: "Apple Macbook", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10), active: true)
+Product.create(category: book, name: "Harry Potter", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10), active: true)
+Product.create(category: fasion, name: "T-Shirt", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10), active: true)
+Product.create(category: toys, name: "Lego Army", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10), active: true)
+Product.create(category: sport, name: "Nike Vapor", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10), active: true)
+Product.create(category: art, name: "Kossak", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10), active: true)
+Product.create(category: ent, name: "Justin Bieber", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10), active: true)
+Product.create(category: home, name: "Modern Lamp", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10), active: true)
+Product.create(category: car, name: "Porshe", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10), active: true)
+Product.create(category: car, name: "Volvo", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10), active: true)
+Product.create(category: car, name: "Skoda", price: Faker::Commerce.price, description: Faker::Lorem.paragraph(2, false, 10), active: true)
 puts "PRODUCTS CREATED"
+
+OrderStatus.create! id: 1, name: "In Progress"
+OrderStatus.create! id: 2, name: "Placed"
+OrderStatus.create! id: 3, name: "Shipped"
+OrderStatus.create! id: 4, name: "Cancelled"
+puts "STATUSES CREATED"
