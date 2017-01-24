6c5781ab1c038d381f09e1ff004f5ddf01cd1b7e
Janek Kurzydlo <janek.kurzydlo@gmail.com>
2017-01-24 21:20:31 +0100

    Create Order

--- test/factories.rb
+++ test/factories.rb
@@ -1,4 +1,11 @@
 FactoryGirl.define do
+  factory :order do
+    subtotal "9.99"
+    tax "9.99"
+    shipping "9.99"
+    total "9.99"
+    order_status nil
+  end
   factory :order_status do
     name "MyString"
   end
