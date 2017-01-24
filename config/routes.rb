58af04573e278669d7b20f5602efb6988ac866c7
Janek Kurzydlo <janek.kurzydlo@gmail.com>
2017-01-24 21:33:21 +0100

    Create carts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'carts/show'
+
 	devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: "welcome#index"
   resources :products
